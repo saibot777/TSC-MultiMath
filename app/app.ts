b3141f8fb4533ae8ef281b0f2a721a446393ecf5
saibot777 <stefantrajkovic777@gmail.com>
2017-05-23 14:00:45 +0200

    compiling configured

---
+++ app/app.ts
@@ -0,0 +1,6 @@
+function startGame () {
+    let messageElement = document.getElementById('messages');
+    messageElement.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+document.getElementById('startGame').addEventListener('click', startGame);
