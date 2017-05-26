9b7d247586bc014db114a4b9207bc0ae4ceeb3ad
stefan.trajkovic <stefan.trajkovic@bluecouchdigital.com>
2017-05-26 09:57:48 +0200

    addional functions added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 function startGame() {
     var playerName = getInputValue('playername');
     logPlayer(playerName);
-    postScore(100, playerName);
+    postScore(80, playerName);
+    postScore(-5, playerName);
 }
 function logPlayer(name) {
-    if (name === void 0) { name = 'Frenzie Player'; }
+    if (name === void 0) { name = 'MultiMath Player'; }
     console.log("New game starting for player: " + name);
 }
 function postScore(score, playerName) {
+    if (playerName === void 0) { playerName = 'MultiMath Player'; }
+    var logger;
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
     var scoreElement = document.getElementById('postedScores');
     scoreElement.innerText = score + " - " + playerName;
+    logger("Score: " + score);
 }
 function getInputValue(elementID) {
     var inputElement = document.getElementById(elementID);
@@ -21,4 +31,8 @@ function getInputValue(elementID) {
     }
 }
 document.getElementById('startGame').addEventListener('click', startGame);
+var logMessage = function (message) { return console.log(message); };
+function logError(error) {
+    console.error(error);
+}
 //# sourceMappingURL=app.js.map
